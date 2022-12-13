615a33d341f63097ddc8d2c94a14d0b7824618e3
Alex Jansen <alexjansen210@gmail.com>
2022-12-12 17:37:29 -0800

    headless and cron shite

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import { Builder, By, Key, until } from 'selenium-webdriver'
+import chrome from 'selenium-webdriver/chrome.js'
 import nodemailer from 'nodemailer'
 import fs from 'fs'
 import cron from 'node-cron'
@@ -23,12 +24,24 @@ transporter.verify(function (error, success) {
         process.exit(1)
     } else {
         console.log("Transporter is ready to take messages.")
+        task.start();
+        console.log("Cron job started.")
     }
 })
 
-cron.schedule('0 10 * * *', async () => {
+if (!cron.validate(process.env.CRON)) {
+    console.log("Cron string is invalid.")
+    process.exit(1)
+} else {
+    console.log('Cron job readied for ' + process.env.CRON)
+}
+
+let task = cron.schedule(process.env.CRON, async () => {
     try {
-        const driver = new Builder().forBrowser("chrome").build()
+        const driver = new Builder()
+            .forBrowser("chrome")
+            .setChromeOptions(new chrome.Options().headless())
+            .build()
 
         await driver.get('https://secure2.tradeschoolinc.com/v5/psejatc-org/login/index.php')
         await driver.findElement(By.id('loginemail')).sendKeys(process.env.JATC_EMAIL)
@@ -39,10 +52,10 @@ cron.schedule('0 10 * * *', async () => {
         const captchaSolution = Number(numbers[0]) + Number(numbers[1])
         await driver.findElement(By.id('captcha')).sendKeys(captchaSolution, Key.ENTER)
 
-        const button = await driver.wait(until.elementLocated(By.name('applications')),30000)
+        const button = await driver.wait(until.elementLocated(By.name('applications')), 30000)
         await button.click()
 
-        const table = await driver.wait(until.elementLocated(By.id('applications')),30000)
+        const table = await driver.wait(until.elementLocated(By.id('applications')), 30000)
         const rank = await table.findElement(By.css('tr:last-child > td:nth-child(7)')).getText()
 
         await driver.quit()
@@ -52,8 +65,10 @@ cron.schedule('0 10 * * *', async () => {
             text: 'You are currently at position ' + rank + '.',
             subject: 'PSEJATC Rank Update'
         })
+        let msg = new Date().toISOString() + " " + rank + "\n"
 
-        logger.write(new Date().toISOString() + " " + rank + "\n")
+        logger.write(msg)
+        console.log(msg)
     }
     catch (e) {
         let info = await transporter.sendMail({
@@ -62,6 +77,6 @@ cron.schedule('0 10 * * *', async () => {
             subject: 'PSEJATC Rank Update Error'
         })
     }
+}, {
+    scheduled: false
 })
-
-console.log("Cron readied 'At 10:00'.")
